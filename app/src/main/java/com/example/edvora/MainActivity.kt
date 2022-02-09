1372f06d1dd1df244054ce0e9759a5c215066d46
Mustafa Raafat <mustafa.raft@hotmail.com>
2022-02-09 15:18:16 +0200

    Update activity_Layout

--- app/src/main/java/com/example/edvora/MainActivity.kt
+++ app/src/main/java/com/example/edvora/MainActivity.kt
@@ -1,11 +1,194 @@
 package com.example.edvora
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RelativeLayout
 
 class MainActivity : AppCompatActivity() {
+//    val dv = DrawingView(this)
+    private lateinit var mPaint: Paint
+    lateinit var d2: RelativeLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    public class DrawingView(context: Context) : View(context) {
+
+        var mywidth: Int = 0
+        var myheight: Int = 0
+        lateinit var mBitmap: Bitmap
+        lateinit var mCanvas: Canvas
+        lateinit var mPath: Path
+        lateinit var mBitmapPaint: Paint
+        lateinit var circlePaint: Paint
+        lateinit var circlePath: Path
+//        Context context;
+//        private Paint recPaint;
+/*
+        public DrawingView(Context c) {
+            super(c);
+            context=c;
+            mPath = new Path();
+//            recPaint=new Paint();
+//            recPaint.setColor(Color.BLACK);
+//            recPaint.setStyle(Paint.Style.STROKE);
+//            recPaint.setStrokeJoin(Paint.Join.MITER);
+//            recPaint.setStrokeWidth(4f);
+            mBitmapPaint = new Paint(Paint.DITHER_FLAG);
+            circlePaint = new Paint();
+            circlePath = new Path();
+            circlePaint.setAntiAlias(true);
+            circlePaint.setColor(Color.BLUE);
+            circlePaint.setStyle(Paint.Style.STROKE);
+            circlePaint.setStrokeJoin(Paint.Join.MITER);
+            circlePaint.setStrokeWidth(4f);
+        }
+
+    }
+
+*/
+        /*
+
+
+    ImageView d;
+    RelativeLayout d2;
+    ConstraintLayout ss;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        dv = new DrawingView(this);
+        setContentView(R.layout.activity_main);
+        d=findViewById(R.id.imageView);
+        d2=findViewById(R.id.imagedd);
+        ss=findViewById(R.id.sss);
+        d2.addView(dv);
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setColor(Color.GREEN);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setStrokeWidth(12);
+    }
+
+    public class DrawingView extends View {
+
+        public int width;
+        public  int height;
+        private Bitmap  mBitmap;
+        private Canvas  mCanvas;
+        private Path    mPath;
+        private Paint   mBitmapPaint;
+        Context context;
+        private Paint circlePaint;
+//        private Paint recPaint;
+        private Path circlePath;
+
+        public DrawingView(Context c) {
+            super(c);
+            context=c;
+            mPath = new Path();
+//            recPaint=new Paint();
+//            recPaint.setColor(Color.BLACK);
+//            recPaint.setStyle(Paint.Style.STROKE);
+//            recPaint.setStrokeJoin(Paint.Join.MITER);
+//            recPaint.setStrokeWidth(4f);
+            mBitmapPaint = new Paint(Paint.DITHER_FLAG);
+            circlePaint = new Paint();
+            circlePath = new Path();
+            circlePaint.setAntiAlias(true);
+            circlePaint.setColor(Color.BLUE);
+            circlePaint.setStyle(Paint.Style.STROKE);
+            circlePaint.setStrokeJoin(Paint.Join.MITER);
+            circlePaint.setStrokeWidth(4f);
+        }
+
+        @Override
+        protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+            super.onSizeChanged(w, h, oldw, oldh);
+
+            mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            mCanvas = new Canvas(mBitmap);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+
+            canvas.drawBitmap( mBitmap, 0, 0, mBitmapPaint);
+            canvas.drawPath( mPath,  mPaint);
+            canvas.drawPath( circlePath,  circlePaint);
+        }
+
+        private float mX, mY;
+        private static final float TOUCH_TOLERANCE = 4;
+
+        private void touch_start(float x, float y) {
+            mPath.reset();
+            mPath.moveTo(x, y);
+            mX = x;
+            mY = y;
+        }
+
+        private void touch_move(float x, float y) {
+            float dx = Math.abs(x - mX);
+            float dy = Math.abs(y - mY);
+            if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+                mPath.quadTo(mX, mY, (x + mX)/2, (y + mY)/2);
+                mX = x;
+                mY = y;
+
+//                mCanvas.drawRect(mX,mY,x,y,recPaint);
+                circlePath.reset();
+                circlePath.addCircle(mX, mY, 30, Path.Direction.CW);
+            }
+        }
+
+        private void touch_up() {
+            mPath.lineTo(mX, mY);
+            circlePath.reset();
+            // commit the path to our offscreen
+            mCanvas.drawPath(mPath,  mPaint);
+            // kill this so we don't double draw
+            mPath.reset();
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            float x = event.getX();
+            float y = event.getY();
+
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    touch_start(x, y);
+                    invalidate();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    touch_move(x, y);
+                    invalidate();
+                    break;
+                case MotionEvent.ACTION_UP:
+                    touch_up();
+                    invalidate();
+                    break;
+            }
+            return true;
+        }
+    }
+    *  */
+//    sd
+//    sdsd
+//    sdsd
+
     }
 }
