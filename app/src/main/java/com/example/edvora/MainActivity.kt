6f16873147c7a920cffe1ed2fe73df851b67b598
Mustafa <mustafa.raft@hotmail.com>
2022-02-10 10:10:42 +0200

    implemented the pencil draawing V1.0

--- app/src/main/java/com/example/edvora/MainActivity.kt
+++ app/src/main/java/com/example/edvora/MainActivity.kt
@@ -1,66 +1,155 @@
 package com.example.edvora
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
+import android.widget.ImageView
 import android.widget.RelativeLayout
 
 class MainActivity : AppCompatActivity() {
-//    val dv = DrawingView(this)
-    private lateinit var mPaint: Paint
+
+    lateinit var imageView:ImageView
+    lateinit var mPaint: Paint
     lateinit var d2: RelativeLayout
+
+
+     fun getf():Paint{
+         if (mPaint==null){
+             mPaint= Paint()
+         }
+        return mPaint;
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mPaint = Paint()
+        var dv= DrawingView(this,mPaint)
+        d2=findViewById(R.id.container_draw);
+        d2.addView(dv)
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setColor(Color.GREEN);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setStrokeWidth(12f);
     }
 
 
-    public class DrawingView(context: Context) : View(context) {
+    public class DrawingView : View {
 
         var mywidth: Int = 0
         var myheight: Int = 0
         lateinit var mBitmap: Bitmap
-        lateinit var mCanvas: Canvas
-        lateinit var mPath: Path
-        lateinit var mBitmapPaint: Paint
-        lateinit var circlePaint: Paint
+         var mCanvas: Canvas
+         var mPath: Path
+         var mmPaint:Paint
+         var mBitmapPaint: Paint
+         var circlePaint: Paint
         lateinit var circlePath: Path
-//        Context context;
-//        private Paint recPaint;
-/*
-        public DrawingView(Context c) {
-            super(c);
-            context=c;
-            mPath = new Path();
-//            recPaint=new Paint();
-//            recPaint.setColor(Color.BLACK);
-//            recPaint.setStyle(Paint.Style.STROKE);
-//            recPaint.setStrokeJoin(Paint.Join.MITER);
-//            recPaint.setStrokeWidth(4f);
-            mBitmapPaint = new Paint(Paint.DITHER_FLAG);
-            circlePaint = new Paint();
-            circlePath = new Path();
+        var mContext : Context
+        lateinit var recPaint: Paint
+        constructor(context: Context,mPaint: Paint): super(context){
+            mPath= Path()
+            mCanvas=Canvas()
+            mmPaint=mPaint
+            circlePath=Path()
+            this.mContext=context
+            mBitmapPaint=Paint(Paint.DITHER_FLAG)
+            circlePaint= Paint()
             circlePaint.setAntiAlias(true);
             circlePaint.setColor(Color.BLUE);
             circlePaint.setStyle(Paint.Style.STROKE);
             circlePaint.setStrokeJoin(Paint.Join.MITER);
             circlePaint.setStrokeWidth(4f);
+//            recPaint=new Paint();
+//            recPaint.setColor(Color.BLACK);
+//            recPaint.setStyle(Paint.Style.STROKE);
+//            recPaint.setStrokeJoin(Paint.Join.MITER);
+//            recPaint.setStrokeWidth(4f);
         }
 
-    }
+        override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+            super.onSizeChanged(w, h, oldw, oldh)
 
-*/
+            mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            mCanvas = Canvas(mBitmap);
+        }
+
+        override fun onDraw(canvas: Canvas) {
+            super.onDraw(canvas)
+
+            canvas.drawBitmap(mBitmap,0f,0f,mBitmapPaint)
+
+            canvas.drawPath( mPath, mmPaint )
+            canvas.drawPath( circlePath,  circlePaint)
+        }
+
+
+        private  var mX=0f
+        private  var mY=0f
+
+        val TOUCH_TOLERANCE = 4f;
+
+        private fun touch_start(x:Float, y:Float) {
+            mPath.reset();
+            mPath.moveTo(x, y);
+            mX = x;
+            mY = y;
+        }
+
+        private fun touch_move( x:Float, y:Float) {
+            var dx = Math.abs(x - mX);
+            var dy = Math.abs(y - mY);
+            if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+                mPath.quadTo(mX, mY, (x + mX)/2, (y + mY)/2);
+                mX = x;
+                mY = y;
+
+//                mCanvas.drawRect(mX,mY,x,y,recPaint);
+                circlePath.reset();
+                circlePath.addCircle(mX, mY, 30f, Path.Direction.CW);
+            }
+        }
+
+        private fun touch_up() {
+            mPath.lineTo(mX, mY);
+            circlePath.reset();
+            // commit the path to our offscreen
+            mCanvas.drawPath(mPath,  mmPaint)
+            // kill this so we don't double draw
+            mPath.reset()
+        }
+
+        override fun onTouchEvent(event: MotionEvent?): Boolean {
+            var x=0f
+            var y=0f
+            if (event != null) {
+                x = event.getX()
+                y = event.getY()
+
+                when (event.getAction()) {
+                    MotionEvent.ACTION_DOWN->{
+                        touch_start(x, y);
+                        invalidate();
+                    }
+                    MotionEvent.ACTION_MOVE->{
+                        touch_move(x, y);
+                        invalidate();
+                    }
+                    MotionEvent.ACTION_UP->{
+                        touch_up();
+                        invalidate();
+                    }
+                }
+            }
+            return true;
+        }
+
         /*
-
-
-    ImageView d;
-    RelativeLayout d2;
-    ConstraintLayout ss;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,35 +172,6 @@ class MainActivity : AppCompatActivity() {
 
     public class DrawingView extends View {
 
-        public int width;
-        public  int height;
-        private Bitmap  mBitmap;
-        private Canvas  mCanvas;
-        private Path    mPath;
-        private Paint   mBitmapPaint;
-        Context context;
-        private Paint circlePaint;
-//        private Paint recPaint;
-        private Path circlePath;
-
-        public DrawingView(Context c) {
-            super(c);
-            context=c;
-            mPath = new Path();
-//            recPaint=new Paint();
-//            recPaint.setColor(Color.BLACK);
-//            recPaint.setStyle(Paint.Style.STROKE);
-//            recPaint.setStrokeJoin(Paint.Join.MITER);
-//            recPaint.setStrokeWidth(4f);
-            mBitmapPaint = new Paint(Paint.DITHER_FLAG);
-            circlePaint = new Paint();
-            circlePath = new Path();
-            circlePaint.setAntiAlias(true);
-            circlePaint.setColor(Color.BLUE);
-            circlePaint.setStyle(Paint.Style.STROKE);
-            circlePaint.setStrokeJoin(Paint.Join.MITER);
-            circlePaint.setStrokeWidth(4f);
-        }
 
         @Override
         protected void onSizeChanged(int w, int h, int oldw, int oldh) {
@@ -186,9 +246,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
     *  */
-//    sd
-//    sdsd
-//    sdsd
 
     }
 }
